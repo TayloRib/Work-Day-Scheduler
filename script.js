5a5be750f5fcb3fc8e5a5866a4ba63ab92f41315
Ribbit <taylor.orsini@yahoo.com>
2023-04-05 11:44:38 -0700

    fixed bug that was rendering future color incorrectly. got help condensing color set function to for loop. functionality done

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ $(function () {
 
     
     var timeCurrent = dayjs().format("HH");
-
+    var hourBlocks = $(".time-block")
+ 
 //show stored activity 
 function showActivity(){
     var activity9 = JSON.parse(localStorage.getItem("activity9"));
@@ -49,160 +50,89 @@ function showActivity(){
     var activity17 = JSON.parse(localStorage.getItem("activity17"));
     savedText17.text(activity17);
 
-    
-    function setColor9() {
-        if (timeCurrent > 9) {
-            $('#hour-9').addClass('past');
-        } else if (timeCurrent = 9) {
-            $('#hour-9').addClass('present');
-        } else {
-            $('#hour-9').addClass('future');
-        }
-    };
-    function setColor10() {
-        if (timeCurrent > 10) {
-            $('#hour-10').addClass('past');
-        } else if (timeCurrent = 10) {
-            $('#hour-10').addClass('present');
-        } else {
-            $('#hour-10').addClass('future');
-        }
-    };
-    function setColor11() {
-        if (timeCurrent > 11) {
-            $('#hour-11').addClass('past');
-        } else if (timeCurrent = 11) {
-            $('#hour-11').addClass('present');
-        } else {
-            $('#hour-11').addClass('future');
-        }
-    };
-    function setColor12() {
-        if (timeCurrent > 12) {
-            $('#hour-12').addClass('past');
-        } else if (timeCurrent = 12) {
-            $('#hour-12').addClass('present');
-        } else {
-            $('#hour-12').addClass('future');
-        }
-    };
-    function setColor13() {
-        if (timeCurrent > 13) {
-            $('#hour-13').addClass('past');
-        } else if (timeCurrent = 13) {
-            $('#hour-13').addClass('present');
-        } else {
-            $('#hour-13').addClass('future');
-        }
-    };
-    function setColor14() {
-        if (timeCurrent > 14) {
-            $('#hour-14').addClass('past');
-        } else if (timeCurrent = 14) {
-            $('#hour-14').addClass('present');
-        } else {
-            $('#hour-14').addClass('future');
-        }
-    };
-    function setColor15() {
-        if (timeCurrent > 15) {
-            $('#hour-15').addClass('past');
-        } else if (timeCurrent = 15) {
-            $('#hour-15').addClass('present');
-        } else {
-            $('#hour-15').addClass('future');
-        }
-    };
-    function setColor16() {
-        if (timeCurrent > 16) {
-            $('#hour-16').addClass('past');
-        } else if (timeCurrent = 16) {
-            $('#hour-16').addClass('present');
-        } else {
-            $('#hour-16').addClass('future');
-        }
-    };
-    function setColor17() {
-        if (timeCurrent > 17) {
-            $('#hour-17').addClass('past');
-        } else if (timeCurrent = 17) {
-            $('#hour-17').addClass('present');
-        } else {
-            $('#hour-17').addClass('future');
-        }
-    };
-
-    setColor9();
-    setColor10();
-    setColor11();
-    setColor12();
-    setColor13();
-    setColor14();
-    setColor15();
-    setColor16();
-    setColor17();
+  
+    function timeCompare(){
+            // hourblocks is an array of all elements that have the "time-block" class
+            // on each of those elements, I want to run a function
+        hourBlocks.each(function() {
+            // the fucntion looks for that element's child div element and takes the id of it
+            // the parseInt turns that ID into a number and assigns it to a variable
+            var blockTime =parseInt( $(this).children('div').attr("id"));
+            // the timeCurrent variable is compared to the assigned variable,
+            // and depending on the comparator, it applies a class
+            // "this" refers to the div element that has the time-block class
+            if (timeCurrent > blockTime) {
+                $(this).children(".description").addClass('past');
+            } else if (timeCurrent >=  blockTime) {
+                $(this).children(".description").addClass('present');
+            } else {
+                $(this).children(".description").addClass('future');
+            }
+        });
+    }
+    timeCompare();
 }
 
 //store the input and time
     saveBtn9.on('click', function(){
         var eventTextNine = $("#eventtext9").val();
-        var timeNine = $("#nine").text();
+        var timeNine = $("#9").text();
 
         localStorage.setItem("timeslot9", JSON.stringify(timeNine));
         localStorage.setItem("activity9", JSON.stringify(eventTextNine));
     });
     saveBtn10.on('click', function(){
         var eventTextTen = $("#eventtext10").val();
-        var timeTen = $("#ten").text();
+        var timeTen = $("#10").text();
 
         localStorage.setItem("timeslot10", JSON.stringify(timeTen))
         localStorage.setItem("activity10", JSON.stringify(eventTextTen))
     });
     saveBtn11.on('click', function(){
         var eventText11 = $("#eventtext11").val();
-        var timeEleven = $("#eleven").text();
+        var timeEleven = $("#11").text();
 
         localStorage.setItem("timeslot11", JSON.stringify(timeEleven))
         localStorage.setItem("activity11", JSON.stringify(eventText11))
     });
     saveBtn12.on('click', function(){
         var eventText12 = $("#eventtext12").val();
-        var timeTwelve = $("#twelve").text();
+        var timeTwelve = $("#12").text();
 
         localStorage.setItem("timeslot12", JSON.stringify(timeTwelve))
         localStorage.setItem("activity12", JSON.stringify(eventText12))
     });
     saveBtn13.on('click', function(){
         var eventText13 = $("#eventtext13").val();
-        var timeThirteen = $("#thirteen").text();
+        var timeThirteen = $("#13").text();
 
         localStorage.setItem("timeslot13", JSON.stringify(timeThirteen))
         localStorage.setItem("activity13", JSON.stringify(eventText13))
     });
     saveBtn14.on('click', function(){
         var eventText14 = $("#eventtext14").val();
-        var timeFourteen = $("#fourteen").text();
+        var timeFourteen = $("#14").text();
 
         localStorage.setItem("timeslot14", JSON.stringify(timeFourteen))
         localStorage.setItem("activity14", JSON.stringify(eventText14))
     });
     saveBtn15.on('click', function(){
         var eventText15 = $("#eventtext15").val();
-        var timeFifteen = $("#fifteen").text();
+        var timeFifteen = $("#15").text();
 
         localStorage.setItem("timeslot15", JSON.stringify(timeFifteen))
         localStorage.setItem("activity15", JSON.stringify(eventText15))
     });
     saveBtn16.on('click', function(){
         var eventText16 = $("#eventtext16").val();
-        var timeSixteen = $("#sixteen").text();
+        var timeSixteen = $("#16").text();
 
         localStorage.setItem("timeslot16", JSON.stringify(timeSixteen))
         localStorage.setItem("activity16", JSON.stringify(eventText16))
     });
     saveBtn17.on('click', function(){
         var eventText17 = $("#eventtext17").val();
-        var timeSeventeen = $("#seventeen").text();
+        var timeSeventeen = $("#17").text();
 
         localStorage.setItem("timeslot17", JSON.stringify(timeSeventeen))
         localStorage.setItem("activity17", JSON.stringify(eventText17))
